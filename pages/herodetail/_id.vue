27674aa6fba7f2760594f0d5b664aaf20dfcfeaf
DESKTOP-O936DB6\asus <992049747@qq.com>
2020-04-25 09:50:02 +0800

    4-25

---
+++ pages/herodetail/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- 头部导航栏 -->
+    <commonNav></commonNav>
+    <!-- 头部广告 -->
+    <topact></topact>
+  </div>
+</template>
+
+<script>
+import commonNav from "../../components/index/commonNav";
+import topact from "../../components/index/topact";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    commonNav,
+    topact
+  },
+  methods: {},
+  mounted() {
+      console.log(this.$route.query.heroId);
+  },
+  watch: {},
+  computed: {},
+  filters: {}
+};
+</script>
+
+<style scoped lang='scss'>
+</style>
